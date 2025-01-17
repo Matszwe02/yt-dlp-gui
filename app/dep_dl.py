f1427bdd5e25f54bdc3d04d4b2ff70616fcf2b9a
Matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2025-01-17 20:28:37 +0100

    Channge ffmpeg download source to official ones

--- app/dep_dl.py
+++ app/dep_dl.py
@@ -5,6 +5,8 @@ import stat
 from io import StringIO
 from tempfile import NamedTemporaryFile
 from datetime import datetime, timedelta
+import tarfile
+import zipfile
 
 import requests
 from PySide6.QtCore import QThread, QTimer, Signal
@@ -20,14 +22,35 @@ os.environ['PATH'] = os.pathsep.join([os.path.join(os.getcwd(), BIN), os.environ
 
 
 def if_download_file(file_path):
-    if not os.path.exists(file_path):
+    # if not os.path.exists(file_path):
+    if not shutil.which(file_path):
         return True
-    
-    current_date = datetime.now()
-    one_month_ago = current_date - timedelta(days=30)
-    mod_time = datetime.fromtimestamp(os.path.getmtime(file_path))
-    return mod_time < one_month_ago
+    try:
+        current_date = datetime.now()
+        one_month_ago = current_date - timedelta(days=30)
+        mod_time = datetime.fromtimestamp(os.path.getmtime(file_path))
+        return mod_time < one_month_ago
+    except:
+        return True
 
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+    
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    # Is the error an access error?
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
 
 class DownloadWindow(QWidget, Ui_Download):
     finished = Signal()
@@ -49,42 +72,45 @@ class DownloadWindow(QWidget, Ui_Download):
     def get_missing_dep(self):
         binaries = {
             "Linux": {
-                "ffmpeg": "ffmpeg-linux64-v4.1",
-                "ffprobe": "ffprobe-linux64-v4.1",
-                "yt-dlp": "yt-dlp_linux",
+                "ffmpeg": "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl-shared.tar.xz",
+                "yt-dlp": "https://github.com/yt-dlp/yt-dlp/releases/latest/yt-dlp_linux",
             },
             "Darwin": {
-                "ffmpeg": "ffmpeg-osx64-v4.1",
-                "ffprobe": "ffprobe-osx64-v4.1",
-                "yt-dlp": "yt-dlp_macos",
+                "ffmpeg": ["https://evermeet.cx/ffmpeg/get/zip", "https://evermeet.cx/ffmpeg/getrelease/ffprobe/zip"],
+                "yt-dlp": "https://github.com/yt-dlp/yt-dlp/releases/latest/yt-dlp_macos",
             },
             "Windows": {
-                "ffmpeg": "ffmpeg-win64-v4.1.exe",
-                "ffprobe": "ffprobe-win64-v4.1.exe",
-                "yt-dlp": "yt-dlp.exe",
+                "ffmpeg": "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip",
+                "yt-dlp": "https://github.com/yt-dlp/yt-dlp/releases/latest/yt-dlp.exe",
             },
         }
 
-        exes = [exe for exe in ["ffmpeg", "ffprobe", "yt-dlp"] if not if_download_file(exe)]
+        exes = [exe for exe in ["ffmpeg", "ffprobe", "yt-dlp"] if if_download_file(exe)]
         os_ = platform.system()
 
         if exes:
+            if os.path.exists(BIN):
+                shutil.rmtree(BIN, ignore_errors=False, onerror=onerror)
             if not os.path.exists(BIN):
                 os.makedirs(BIN)
-            for exe in exes:
-                if exe == "yt-dlp":
-                    url = (
-                        "https://github.com/yt-dlp/yt-dlp/releases/latest/download/"
-                        + binaries[os_][exe]
-                    )
-                else:
-                    url = (
-                        "https://github.com/imageio/imageio-binaries/raw/master/ffmpeg/"
-                        + binaries[os_][exe]
-                    )
-                filename = os.path.join(BIN, f"{exe}.exe" if os_ == "Windows" else exe)
-
+                
+            url = binaries[os_]['yt-dlp']
+            filename = os.path.join(BIN, f"yt-dlp.exe" if os_ == "Windows" else 'yt-dlp')
+            self.missing += [[url, filename]]
+            
+            if os_ == 'Darwin':
+                url = binaries[os_]['ffmpeg'][0]
+                filename = os.path.join(BIN, "ffmpeg_archive.zip")
                 self.missing += [[url, filename]]
+                url = binaries[os_]['ffmpeg'][1]
+                filename = os.path.join(BIN, "ffprobe_archive.zip")
+                self.missing += [[url, filename]]
+            
+            else:
+                url = binaries[os_]['ffmpeg']
+                filename = os.path.join(BIN, "ffmpeg_archive." + ('zip' if os_ == 'Windows' else 'tar.xz'))
+                self.missing += [[url, filename]]
+            
 
     def download_init(self):
         url, filename = self.missing[0]
@@ -96,8 +122,12 @@ class DownloadWindow(QWidget, Ui_Download):
 
     def on_download_finished(self):
         url, filename = self.missing.pop(0)
-        st = os.stat(filename)
-        os.chmod(filename, st.st_mode | stat.S_IEXEC)
+        
+        if 'ff' in url.lower():
+            self.extract_ffmpeg(filename)
+        else:
+            st = os.stat(filename)
+            os.chmod(filename, st.st_mode | stat.S_IEXEC)
 
         if self.missing:
             self.download_init()
@@ -108,6 +138,36 @@ class DownloadWindow(QWidget, Ui_Download):
         self.pb.setValue(progress)
         self.lb_progress.setText(data)
 
+
+    def extract_ffmpeg(self, archive_path):
+        if archive_path.endswith('.zip'):
+            with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+                zip_ref.extractall(BIN)
+        elif archive_path.endswith('.tar.xz'):
+            with tarfile.open(archive_path, 'r:xz') as tar_ref:
+                tar_ref.extractall(BIN)
+        else:
+            raise ValueError(f"Unsupported archive format: {archive_path}")
+
+        # Search for ffmpeg and ffprobe executables
+        for root, dirs, files in os.walk(os.path.join(BIN)):
+            for file in files:
+                if file.lower() in ['ffprobe', 'ffmpeg', 'ffprobe.exe', 'ffmpeg.exe'] and os.path.split(root)[-1] == 'bin':
+                    src = os.path.join(root, file)
+                    dst = os.path.join(BIN, file)
+                    if src != dst:
+                        shutil.copy(src, dst)
+                        os.chmod(dst, stat.S_IEXEC)
+
+    # Remove all directories inside BIN and the archive itself
+        for item in os.listdir(BIN):
+            item_path = os.path.join(BIN, item)
+            if os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+            elif item == os.path.basename(archive_path):
+                os.remove(item_path)
+
+    
 
 class _D_Worker(QThread):
     progress = Signal(int, str)
